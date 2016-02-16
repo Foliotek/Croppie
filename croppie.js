696247e368a1e7c2892713605b130cf1010d462a
Alexis Mangin <me@alexismangin.com>
2016-02-15 21:19:56 -0500

    Fix issue with zoom wheel not working on Mac and iOS devices when the zoomer is not displayed.

--- croppie.js
+++ croppie.js
@@ -871,7 +871,9 @@
         this.options = deepExtend(deepExtend({}, Croppie.defaults), opts);
 
         // backwards compatibility
-        this.options.enableZoom = this.options.showZoomer;
+        if (typeof(opts.showZoomer) !== 'undefined') {
+            this.options.enableZoom = this.options.showZoomer = opts.showZoomer;
+        }
 
         _create.call(this);
     }
