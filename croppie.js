4eee576eb39c89305cbb2c7d4d91e5e881bb62f7
Dustin Smith <thedustinsmith@gmail.com>
2016-08-05 13:47:21 -0500

    prep for 2.3.0

--- croppie.js
+++ croppie.js
@@ -2,7 +2,7 @@
  * Croppie
  * Copyright 2016
  * Foliotek
- * Version: 2.2.0
+ * Version: 2.3.0
  *************************/
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
