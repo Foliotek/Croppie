aceb8fe3a8e3689939425038406b335c065b8ae1
David Lee <hoanglm26@gmail.com>
2018-05-22 10:27:27 +0700

    Update croppie.js
    Fix bug crop wrong width & height
    self._originalImageWidth = img.width;
    self._originalImageHeight = img.height;

--- croppie.js
+++ croppie.js
@@ -1015,8 +1015,8 @@
 
         imgData = self.elements.preview.getBoundingClientRect();
 
-        self._originalImageWidth = imgData.width;
-        self._originalImageHeight = imgData.height;
+        self._originalImageWidth = img.width;
+        self._originalImageHeight = img.height;
         self.data.orientation = getExifOrientation(self.elements.img);
 
         if (self.options.enableZoom) {
