175903b128d2704c92e103c17927897ba39d32bf
Zach Dennis <zach.dennis@gmail.com>
2019-01-19 10:26:06 -0500

    Resolve cropping issues when enforceBoundary is set to false (#592)
    This change-set ensures Croppie does not attempt to use image data outside of an image’s bounds. This also ensures that the destination x/y and width/height are scaled appropriately so the image is placed where one would expect.

--- croppie.js
+++ croppie.js
@@ -1178,13 +1178,53 @@
             ctx.fillRect(0, 0, canvasWidth, canvasHeight);
         }
 
-        if (self.options.enforceBoundary !== false) {
-            width = Math.min(width, self._originalImageWidth);
-            height = Math.min(height, self._originalImageHeight);
+        // By default assume we're going to draw the entire
+        // source image onto the destination canvas.
+        sx = left;
+        sy = top;
+        sWidth = width;
+        sHeight = height;
+        dx = 0;
+        dy = 0;
+        dWidth = canvasWidth;
+        dHeight = canvasHeight;
+
+        //
+        // Do not go outside of the original image's bounds along the x-axis.
+        // Handle translations when projecting onto the destination canvas.
+        //
+
+        // The smallest possible source x-position is 0.
+        if (left < 0) {
+            sx = 0;
+            dx = (Math.abs(left) / width) * canvasWidth;
         }
-    
-        // console.table({ left, right, top, bottom, canvasWidth, canvasHeight });
-        ctx.drawImage(this.elements.preview, left, top, width, height, startX, startY, canvasWidth, canvasHeight);
+
+        // The largest possible source width is the original image's width.
+        if (sWidth + sx > self._originalImageWidth) {
+            sWidth = self._originalImageWidth - sx;
+            dWidth =  (sWidth / width) * canvasWidth;
+        }
+
+        //
+        // Do not go outside of the original image's bounds along the y-axis.
+        //
+
+        // The smallest possible source y-position is 0.
+        if (top < 0) {
+            sy = 0;
+            dy = (Math.abs(top) / height) * canvasHeight;
+        }
+
+        // The largest possible source height is the original image's height.
+        if (sHeight + sy > self._originalImageHeight) {
+            sHeight = self._originalImageHeight - sy;
+            dHeight = (sHeight / height) * canvasHeight;
+        }
+
+        // console.table({ left, right, top, bottom, canvasWidth, canvasHeight, width, height, startX, startY, circle, sx, sy, dx, dy, sWidth, sHeight, dWidth, dHeight });
+
+        ctx.drawImage(this.elements.preview, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
         if (circle) {
             ctx.fillStyle = '#fff';
             ctx.globalCompositeOperation = 'destination-in';
