68ded098d16a38f7e03b90102585b288ee66446f
Marc Krämer <github@mokraemer.de>
2019-07-01 13:47:06 +0200

    default zoom does not show the whole picture

--- croppie.js
+++ croppie.js
@@ -1091,7 +1091,7 @@
             _setZoomerVal.call(self, scale < zoomer.min ? zoomer.min : zoomer.max);
         }
         else if (initial) {
-            defaultInitialZoom = Math.max((boundaryData.width / imgData.width), (boundaryData.height / imgData.height));
+            defaultInitialZoom = Math.min((boundaryData.width / imgData.width), (boundaryData.height / imgData.height)); //Math.max in Math.min ge\E4ndert
             initialZoom = self.data.boundZoom !== null ? self.data.boundZoom : defaultInitialZoom;
             _setZoomerVal.call(self, initialZoom);
         }
